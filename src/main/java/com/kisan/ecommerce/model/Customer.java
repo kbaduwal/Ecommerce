d478d26cbab90ddaffc30aff1d5f85cbad7a1781
kbaduwal <khakendra1999@gmail.com>
2024-02-07 23:22:54 +0530

    Added the Model package for the Entity to map with MySQL database

---
+++ src/main/java/com/kisan/ecommerce/model/Customer.java
@@ -0,0 +1,81 @@
+package com.kisan.ecommerce.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+
+@Entity
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String imageUrl;
+
+    public Customer(){
+
+    }
+
+    public Customer(String name, String email, String phone, String imageUrl) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.imageUrl = imageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
