a6bdb46d555e1b38ea8af750d366957348a1a20d
kbaduwal <khakendra1999@gmail.com>
2024-02-10 23:26:32 +0530

    Will Be starting tommorow

---
+++ src/main/java/com/kisan/ecommerce/EmployeeResource.java
@@ -0,0 +1,10 @@
+package com.kisan.ecommerce;
+//This is our controller which will response to the different request
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class EmployeeResource {
+}
